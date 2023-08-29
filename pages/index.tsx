cb7434ffa0a8040cfb2044035c489824e0b6d1b9
Hubert Adamkowski <hubertadamkowskip56@gmail.com>
2023-08-29 20:05:47 +0200

    delete layoutPresenter component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,26 @@
-import LayoutPresenter from "../components/LayoutPresenter";
+import NavbarPresenter from "../components/Navbar/NavbarPresenter";
+import FooterPresenter from "../components/Footer/FooterPresenter";
+import ContentSectionPresenter from "../components/ContentSection/OneContentSectionPresenter";
+import FirstSectionPresenter from "../components/ContentSection/FirstSectionPresenter";
+import SecondContentSectionPresenter from "../components/ContentSection/TwoContentSectionPresenter";
+import ThreeContentSectionPresenter from "../components/ContentSection/ThreeContentSectionPresenter";
+import FourContentSectionPresenter from "../components/ContentSection/FourContentSectionPresenter";
+import FiveContentSectionPresenter from "../components/ContentSection/FiveContentSectionPresenter";
+
 const HomePage = () => {
   return (
-    <LayoutPresenter>
-      <h1>Finnet Page </h1>
-    </LayoutPresenter>
+    <div>
+      <NavbarPresenter />
+      <main>
+        <FirstSectionPresenter />
+        <ContentSectionPresenter />
+        <SecondContentSectionPresenter />
+        <ThreeContentSectionPresenter />
+        <FourContentSectionPresenter />
+        <FiveContentSectionPresenter />
+      </main>
+      <FooterPresenter />
+    </div>
   );
 };
 
